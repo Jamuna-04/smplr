09076cc5ba121a9204d8275ac32c3c26fb3556b5
Jamuna-04 <jamunalakshmi2003@gmail.com>
2025-12-10 06:33:13 +0000

    mavenjavacomit

--- laptop.java
+++ laptop.java
@@ -1,3 +1,28 @@
  hello
   updated
-  sourcecode
+
+i  sourcecode
+
+
+
+
+
+
+
+
+
+<groupId>com.lap</groupId>
+<artifactId>maven-java-project</artifactId>
+<version>0.0.1snapshot</version
+<packaging>jar</packaging>
+
+<dependencies>
+
+<dependency>
+<groupId>junit</groupId>
+<artifactId>junit</artifactId>
+<version>3.8.1</version>
+<scope>test<scope>
+</dependency>
+
+</dependencies>
